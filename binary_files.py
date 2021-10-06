1752e7ecdb01c625ce6aa90aee88f721adf7c6ce
Nels885 <voirin.lionel@gmail.com>
2021-10-06 11:19:09 +0200

    Modification of the binary generator to use the most recurring values between each files

--- binary_files.py
+++ binary_files.py
@@ -18,11 +18,11 @@ class BinaryFile:
 
     def compare(self):
         number = error_nb = 0
-        files = [None for f in self.bfc]
+        files = [None for f in self.bfc[:4]]
         try:
             if self.get_size():
                 print("SIZE OK")
-                files = [open(f, "rb") for f in self.bfc]
+                files = [open(f, "rb") for f in self.bfc[:4]]
                 while True:
                     buf = [binascii.hexlify(file.read(self.lgbuf)).decode("utf8").upper() for file in files]
                     if len(buf[0]) == 0:
@@ -53,14 +53,11 @@ class BinaryFile:
                 files = [open(f, "rb") for f in self.bfc]
                 while True:
                     buf = [file.read(2) for file in files]
-                    buf1 = files[0].read(2)
                     if len(buf[0]) == 0:
                         self.result = True
                         break
-                    if buf[0] == buf[1] or buf[0] == buf[2]:
-                        f.write(buf[0])
-                    elif buf[1] == buf[0] or buf[1] == buf[3]:
-                        f.write(buf[1])
+                    max_value = max(buf, key=buf.count)
+                    f.write(max_value)
             [f.close() for f in files]
         except:
             for file in files:
@@ -90,16 +87,18 @@ if __name__ == "__main__":
  
     import time
  
-    bf1 = "dump000.bin"
-    bf2 = "dump001.bin"
-    bf3 = "dump004_ok.bin"
-    bf4 = "dump005_ok.bin"
+    bfiles = [
+        "test3.bin", "dump001.BIN", "dump002.BIN","dump003.BIN", "dump004_ok.BIN", 
+        "dump005_ok.BIN", "dump006.BIN", "dump007.BIN", "dump008.BIN", "dump009.BIN"
+    ]
 
     t = time.process_time()
-    files = BinaryFile(bf1, bf2, bf3, bf4)
+    files = BinaryFile(*bfiles)
     error_nb = files.compare()
     t = time.process_time()-t
     print("Résultat:", files.result, "%.3f s" % t)
     print(f"Nombre d'erreur: {error_nb}")
 
-    # files.generate("test2.bin")
+    # files.generate("test3.bin")
+    # t = time.process_time()-t
+    # print("Generate file terminated:", "%.3f s" % t)
